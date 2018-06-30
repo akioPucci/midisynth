7737a0545dbd09b974711f6e7602f07b48383caf
8957197 <8957197@6-305-07.grad.usricmc.icmc.usp.br>
2018-06-30 16:37:01 -0300

    env added

--- synth01/src/synth/Note.java
+++ synth01/src/synth/Note.java
@@ -7,6 +7,17 @@ public class Note {
 	HashMap<String, Double> currentChannelSample;
 	double currentSample;
 	double currentTime;
+	double releaseStartTime;
+	
+
+
+	double initTime;
+	//envelopes states:
+	//0 - attack
+	//1 - decay
+	//2 - sustain
+	//3 - release
+	int envState;
 	boolean fin;
 	
 	public Note(int noteNumber) {
@@ -14,8 +25,27 @@ public class Note {
 		this.currentChannelSample = new HashMap<String, Double>();
 		this.currentSample = 0;
 		
+		this.initTime = -1;
+		this.envState = 0;
 		this.currentTime = 0;
+		this.releaseStartTime = -1;
 		this.fin = false;
+	}
+	
+	public double getInitTime() {
+		return initTime;
+	}
+
+	public void setInitTime(double initTime) {
+		this.initTime = initTime;
+	}
+	
+	public double getReleaseStartTime() {
+		return releaseStartTime;
+	}
+
+	public void setReleaseStartTime(double releaseTime) {
+		this.releaseStartTime = releaseTime;
 	}
 	
 	public boolean isFin() {
@@ -60,6 +90,28 @@ public class Note {
 	
 	public double getTime() {
 		return this.currentTime;
+	}
+	
+	public int getEnvState() {
+		return this.envState;
+	}
+	
+	public void setEnvState(int state) {
+		switch(state) {
+		case 0:
+			System.out.println("attack");
+			break;
+		case 1:
+			System.out.println("decay");
+			break;
+		case 2:
+			System.out.println("sustain");
+			break;
+		case 3:
+			System.out.println("release");
+			break;
+		}
+		this.envState = state;
 	}
 	
 	
